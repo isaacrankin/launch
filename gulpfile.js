da026db2d347e0b180623288a9f4bb8a2192d001
Isaac Rankin <isaac.webfix@gmail.com>
2016-03-04 12:10:22 +1300

    replaced grunt with gulp and a simpler build - also using babel for es6

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const jshint = require('gulp-jshint');
+const sassLint = require('gulp-sass-lint');
+
+var paths = {
+    dist: './dist',
+    scripts: [
+        './js/lib/*.js',
+        './js/app.js'
+    ],
+    sass: ['./sass/**/*.scss'],
+    // files that simply get copied to dist folder
+    copy: [
+        './fonts',
+        './img',
+        './svg',
+        './.htaccess',
+        './apple-touch-icon.png',
+        './browserconfig.xml',
+        './crossdomain.xml',
+        './favicon.ico',
+        './*.html',
+        './robots.txt',
+        './tile.png',
+        './tile-wide.png'
+    ],
+    // vendor scripts to get combined
+    vendorScripts: [
+        './js/vendor/*.js'
+    ],
+    // vendor scripts that shouldn't be combined
+    copyVendorScripts: [
+        './node_modules/jquery/dist/*'
+    ]
+};
+
+gulp.task('sass', function () {
+    return gulp.src(paths.sass)
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest(`${paths.dist}/css`));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('app.min.js'))
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(gulp.dest(`${paths.dist}/js`));
+});
+
+// Copy files to dist dir
+gulp.task('copy', function () {
+    return gulp.src(paths.copy)
+        .pipe(gulp.dest(paths.dist));
+});
+
+// Concat and ugilfy vendor scripts
+gulp.task('vendor-scripts', function () {
+    return gulp.src(paths.vendorScripts)
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${paths.dist}/js`));
+});
+
+// Copy over vendor scripts that shouldn't be combined
+gulp.task('copy-vendor-scripts', function () {
+    gulp.src(paths.copyVendorScripts)
+        .pipe(gulp.dest(`${paths.dist}/js/vendor`));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.vendorScripts, ['scripts']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.copy, ['copy']);
+});
+
+gulp.task('default', ['sass', 'scripts', 'copy', 'copy-vendor-scripts', 'vendor-scripts']);
--- js/app.js
+++ js/app.js
@@ -1,37 +1,5 @@
-/* global Mediator:false, Lib:false */
-
 'use strict';
 
-var App = {};
-
-(function(window, document, $) {
-
-    $(function(){
-
-        App.mediator = new Mediator();
-
-        //TODO: install a router???
-
-        // Example use of modules
-        // ----------------------------------------------------------------
-
-        // Example ES6 module
-        var example = new Lib.Module('Example ES6 Module');
-        console.log(example.message('using an ES6 method'));
-
-        // Example ES5 module
-        var es5Example = new Lib.ES5Module('Example ES5 Module');
-        console.log(es5Example.message('using an ES5 method'));
-
-        // Channels
-        // ----------------------------------------------------------------
-        App.mediator.subscribe('exampleChannel', function (arg) {
-            console.log(arg);
-        });
-
-        // Initiate Plugins
-        // ----------------------------------------------------------------
-
-    });
-
-})(window, document, jQuery);
+$(function() {
+    console.log('Hello World');
+});
